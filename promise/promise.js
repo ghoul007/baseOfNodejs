05ba582b5c343b165fd19033871a9c06a3176704
ahmed <ahmedgh@rosafian.com>
2018-12-08 12:08:36 +0100

    callback with promise

--- promise/promise.js
+++ promise/promise.js
@@ -1,7 +1,17 @@
-exports.getRandomNumber = (maxValue) => {
+exports.getRandomNumber = (maxValue, callback) => {
+
+    if (callback !== undefined) {
+        setTimeout(() => {
+            const result = Math.floor(Math.random() * maxValue);
+            if (typeof maxValue !== 'number') {
+                return callback(new Error('ivalid type'), null)
+            }
+            return callback(null, result)
+        }, 0);
+    }
     return new Promise((resolve, reject) => {
         if (typeof maxValue !== 'number') {
-           reject(new Error('invalid number'))
+            reject(new Error('invalid number'))
         }
         const result = Math.floor(Math.random() * maxValue);
         resolve(result)
