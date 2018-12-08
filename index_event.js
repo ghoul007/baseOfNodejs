6b5fe1fa6944223e8ed5c07a74683ffa55885df4
ahmed <ahmedgh@rosafian.com>
2018-12-08 14:18:44 +0100

    example with  eventEmitter with multi callback

--- index_event.js
+++ index_event.js
@@ -1,8 +1,22 @@
 const EventEmitter = require('events').EventEmitter;
 const programmer = new EventEmitter();
 
+let a = 5;
 programmer.on('event1', () => {
-    console.log('test event1')
+    a = a + 1;
+    console.log('test event1 ', a)
+})
+
+const cusFct = () => {
+    console.log('test event1.1')
+}
+
+programmer.on('event1', cusFct)
+
+programmer.on('event1', (b = 'test') => {
+    console.log(`send params ${b}`)
 })
 
 programmer.emit('event1');
+programmer.emit('event1');
+programmer.emit('event1', 'ahmed');
