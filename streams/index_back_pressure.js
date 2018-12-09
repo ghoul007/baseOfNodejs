f2f43d2e23f39800432b85b79767a10c3351e012
ahmed <ahmedgh@rosafian.com>
2018-12-09 11:15:03 +0100

    hightWaterMark option in createWriteStream

--- streams/index_back_pressure.js
+++ streams/index_back_pressure.js
@@ -1,11 +1,17 @@
 const { createReadStream, createWriteStream } = require('fs');
 
 const myReadStream = createReadStream('./big.txt');
-const mywriteStream = createWriteStream('./big_copy.txt')
+const mywriteStream = createWriteStream('./big_copy.txt', {
+    highWaterMark: 100000
+})
 
+
+let ngOfPauses = 0;
 myReadStream.on('data', (chunk) => {
     const isReadyToWriteData = mywriteStream.write(chunk);
     if (!isReadyToWriteData) {
+        ngOfPauses = ngOfPauses + 1;
+        console.log("ngOfPauses ", ngOfPauses);
         console.log("isReadyToWriteData ", isReadyToWriteData);
         myReadStream.pause();
     }
