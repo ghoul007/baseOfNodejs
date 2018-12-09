fccc62f047e6bafbbecd1a60eba324a84ef1959f
ahmed <ahmedgh@rosafian.com>
2018-12-09 12:11:32 +0100

    sample server  without stream

---
+++ streams/servers/index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs')
+
+const videoPath = './video/video.mp4';
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    if (req.url === '/favicon') {
+        retutn
+    }
+    if (req.url === '/contact') {
+        memUsage('contact')
+        res.end('contact us')
+    } else if (req.url === '/videos') {
+        fs.readFile(videoPath, (error, data) => {
+            memUsage('vidoe')
+            if (error) {
+                console.log("error ", error.message);
+            }
+            res.writeHead(200, { 'Content-type': 'video/mp4' });
+            res.end(data);
+        })
+    } else {
+        res.end(` ${req.url}`)
+    }
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log("server on port ", port);
+
+})
+
+function memUsage(pageUrl) {
+    const used = process.memoryUsage();
+    console.log("used ", used);
+    for (key in used) {
+        console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} Mo`)
+    }
+}
